7cf9cb2dbb88ff1f76c863e214ba316a01b9544e
mahesh <mail.mahesh.lk@gmail.com>
2021-06-20 13:49:22 +0800

    tiny-go

--- tinygo-wasm/main.go
+++ tinygo-wasm/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-"log"
-"net/http"
-"strings"
+	"log"
+	"net/http"
+	"strings"
 )
 
 const dir = "./html"
@@ -11,11 +11,5 @@ const dir = "./html"
 func main() {
 	fs := http.FileServer(http.Dir(dir))
 	log.Print("Serving " + dir + " on http://localhost:8080")
-	http.ListenAndServe(":8080", http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
-		resp.Header().Add("Cache-Control", "no-cache")
-		if strings.HasSuffix(req.URL.Path, ".wasm") {
-			resp.Header().Set("content-type", "application/wasm")
-		}
-		fs.ServeHTTP(resp, req)
-	}))
+
 }
